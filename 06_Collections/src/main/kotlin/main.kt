3bb14df91b1f75e351cfd5daee4173d43d963807
Pavel Bodrug <plkoij73@gmail.com>
2022-06-07 20:32:29 +0300

    create README.md and edit homework 6

--- 06_Collections/src/main/kotlin/main.kt
+++ 06_Collections/src/main/kotlin/main.kt
@@ -1,18 +1,13 @@
 import kotlin.random.Random
 import kotlin.collections.mutableMapOf as mutableMapOf
 
-
 fun main() {
 
-    var n : Int
-
+    var n: Int
     print("Введите N : ")
-
     do {
         n = readLine()?.toIntOrNull() ?: 0
-
         if (n <= 0) println("Попробуйте еще раз!")
-
     } while (n <= 0)
 
     //телефонная книга
@@ -26,70 +21,58 @@ fun main() {
     println(phoneBook.keys.filter { key -> key.startsWith("+7") })
 
     //выводим количество уникальных телефонов
-    println("""Колличество уникальных телефонов: ${phoneBook.keys.toSet().size}""")
+    println("""Количество уникальных телефонов: ${phoneBook.keys.toSet().size}""")
 
-    //сумма длинн всех номеров телефонов
-    println("""Сумма длинн всех номеров телефонов: ${phoneBook.keys.sumOf { it.length }}""")
+    //сумма длин всех номеров телефонов
+    println("""Сумма длин всех номеров телефонов: ${phoneBook.keys.sumOf { it.length }}""")
 
     //выводим отсортированное по имени
     println("Сортируем по имени:")
-    //println(phoneBook.entries.sortedBy { it.value }.joinToString("\n"))
     for (entry in phoneBook.entries.sortedBy { it.value })
         println("""Абонент: ${entry.value}. Номер телефона: ${entry.key}""")
 
     //выводим отсортированное по номеру телефона
     println("Сортируем по номеру телефона:")
-    //println(phoneBook.entries.sortedBy { it.value }.joinToString("\n"))
     for (entry in phoneBook.entries.sortedBy { it.key })
         println("""Номер телефона: ${entry.key}. Абонент: ${entry.value}""")
 }
 
 //создаем список записей
-fun createPhoneBookList(n : Int) : Map<String, String> {
+fun createPhoneBookList(n: Int): Map<String, String> {
     val iList = mutableMapOf<String, String>()
     for (i in 0 until n) {
-        val pair : Pair<String, String> = Pair(
+        val pair: Pair<String, String> = Pair(
             randomPhone(), if (Random.nextBoolean())
                 randomNamesMan()
-             else
+            else
                 randomNamesWoman()
         )
         iList[pair.first] = pair.second
-
-        /*      iList = if (Random.nextBoolean()) {
-                  iList + Pair(randomPhone(), randomNamesMan())
-              } else
-                  iList + Pair(randomPhone(), randomNamesWoman())
-         */
     }
     return iList
 }
 
 //генерируем номер телефона
-fun randomPhone() : String {
+fun randomPhone(): String {
 
     return "+" + Random.nextInt(7, 9).toString() +
-
             Random.nextInt(900, 999).toString() +
-
             Random.nextInt(100, 999).toString() +
-
             Random.nextInt(1000, 9999).toString()
-
 }
 
 //генерируем имена
-fun randomNamesMan() : String {
+fun randomNamesMan(): String {
     val name = arrayOf("Павел", "Григорий", "Александр", "Михаил", "Дмитрий", "Андрей", "Евгений", "Семён", "Алексей")
     val patronymic = arrayOf("Леонидович", "Владимирович", "Алексеевич", "Анатольевич",
         "Александрович", "Михаилович", "Дмитриевич", "Андреевич")
-    return name[Random.nextInt(0, name.lastIndex)] + " " + patronymic[Random.nextInt(0, patronymic.lastIndex)]
+    return name.random() + " " + patronymic.random()
 }
 
-fun randomNamesWoman() : String {
+fun randomNamesWoman(): String {
     val name =
         arrayOf("Анастасия", "Евгения", "Александра", "Дарья", "Анна", "Екатерина", "Елена", "Маргарита", "Любовь")
     val patronymic = arrayOf("Леонидовна", "Владимировна", "Алексеевна", "Анатольевна",
         "Александровна", "Михаиловна", "Дмитриевна", "Андреевна")
-    return name[Random.nextInt(0, name.lastIndex)] + " " + patronymic[Random.nextInt(0, patronymic.lastIndex)]
+    return name.random() + " " + patronymic.random()
 }
