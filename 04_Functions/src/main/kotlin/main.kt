690fccd5ecd27272cc44010ed44774752e07b15e
Pavel Bodrug <plkoij73@gmail.com>
2022-07-28 21:07:30 +0300

    code optimization

--- 04_Functions/src/main/kotlin/main.kt
+++ 04_Functions/src/main/kotlin/main.kt
@@ -13,41 +13,23 @@ fun main() {
 }
 
 // первая половина шифра
-fun firstHalfString(str: String): String {
-//выбираем первую половину сообщения
-    return str.dropLast(str.length / 2)
-        .map { it+1 }.joinToString("")
-        .replace('5', 's')
-        .replace('4', 'u')
-        .map { it -3 }.joinToString ( "" )
-        .replace('0', 'o')
-}
+fun firstHalfString(str: String) = str.dropLast(str.length / 2)
+    .shift { it + 1 }
+    .replace('5', 's')
+    .replace('4', 'u')
+    .shift { it - 3 }
+    .replace('0', 'o')
 
 // вторая половина шифра
-fun secongHalfString(str: String): String {
-
-    //выбираем вторую половину сообщения
-    var secondHalf: String = str.drop(str.length / 2)
-
-    //разворачиваем
-    secondHalf = secondHalf.reversed()
-
-    //сдвигаем на 4 символа влево
-    secondHalf = shift(secondHalf) { it - 4 }
-
-    //заменяем символ подчеркивания на пробел
-    secondHalf = secondHalf.replace('_', ' ')
-
-    //возвращаем функции значение
-    return secondHalf
-}
+fun secongHalfString(str: String) = str.drop(str.length / 2).reversed()
+    .shift { it - 4 }.replace('_', ' ')
 
 /*функция высшего порядка
 на входе строка и функция
 на выходе строка*/
 
-fun shift(sourceString: String, func: (Char) -> Char): String {
-    return sourceString.map(func).joinToString("")
+fun String.shift(func: (Char) -> Char): String {
+    return this.map(func).joinToString("")
 }
 
 
